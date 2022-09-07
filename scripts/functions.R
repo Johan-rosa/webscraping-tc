dab01f14252fe952027597332a9fd8d2c6f2aeba
Johan-rosa <johan.rosaperez@gmail.com>
2022-09-07 17:56:26 -0400

    Add funcion to download rates from popular, bhd and banreservas

--- scripts/functions.R
+++ scripts/functions.R
@@ -1,35 +1,47 @@
 # Dependecies -------------------------------------------------------------
-box::use(rvest[read_html, html_text, html_element])
-box::use(stringr[str_extract])
-box::use(readr[parse_number])
+box::use(
+  rvest[read_html, html_text, html_element, html_table],
+  stringr[str_extract, str_remove],
+  readr[parse_number],
+  dplyr[...],
+  stats[setNames]
+)
 
 #' Descarga la tasa de cambio de Banreservas
 #' @export
-tasa_dolar_banreservas <- function() {
-  url <- "https://www.banreservas.com/calculadoras/pages/divisas.aspx"
-  page <- read_html(url)
+tasa_dolar_banreservas <- function(selenium_client) {
+  selenium_client$navigate('https://www.banreservas.com/')
+  selenium_client$maxWindowSize()
   
-  buy <- page |>
-    html_element('#edit-buy-wrapper .form-item-wrapper .form-label-text') |>
-    html_text() |>
-    str_extract("RD.+$")
+  tasa_compra <- selenium_client$findElement(
+    using = "css selector", 
+    "#site-nav-panel > ul:nth-child(1) > li:nth-child(2) > span"
+  )
   
-  sell <- page |>
-    html_element('#edit-sell-wrapper .form-item-wrapper .form-label-text') |>
-    html_text() |>
-    str_extract("RD.+$")
+  tasa_venta <- selenium_client$findElement(
+    using = "css selector", 
+    "#site-nav-panel > ul:nth-child(1) > li:nth-child(3) > span"
+  )
+  
+  compra <- tasa_compra$getElementText() %>%
+    unlist() %>% 
+    parse_number()
+  
+  venta <- tasa_venta$getElementText() %>%
+    unlist() %>% 
+    parse_number()
   
   data.frame(
     date = Sys.Date(),
-    bank = 'Banresevas',
-    buy = parse_number(buy),
-    sell = parse_number(sell)
+    bank = "Banreservas",
+    buy = compra,
+    sell = venta
   )
 }
 
 #' Descarga la tasa de cambio de Scotiabank
 #' @export
-tasa_dolar_scotia <- function() {
+tasa_dolar_scotiabank <- function() {
   url <- "https://do.scotiabank.com/banca-personal/tarifas/tasas-de-cambio.html"
   
   read_html(url) %>%
@@ -40,4 +52,71 @@ tasa_dolar_scotia <- function() {
     mutate(tipo = str_remove(tipo, "Dólar (USD) ")) %>%
     mutate(bank = "Scotiabank", date = Sys.Date()) %>%
     select(date, bank, tipo, buy = compra, sell = venta)
+}
+
+#' Descarga la tasa de cambio de Banco Popular
+#' @export
+tasa_dolar_popular <- function(selenium_client) {
+  
+  selenium_client$navigate("https://www.popularenlinea.com/personas/Paginas/Home.aspx")
+  selenium_client$maxWindowSize()
+  
+  tasas_banner <- selenium_client$findElement(
+    using = "css selector", 
+    "#s4-bodyContainer > section.footer_est_bpd.footer_est_personas > nav > ul > li:nth-child(3)"
+  )
+  tasas_banner$clickElement()
+  
+  tasa_compra <- selenium_client$findElement(
+    using = "css selector",
+    "#compra_peso_dolar_desktop")
+  tasa_venta <- selenium_client$findElement(
+    using = "css selector",
+    "#venta_peso_dolar_desktop")
+  
+  compra <- as.numeric(tasa_compra$getElementAttribute("value"))
+  venta <- as.numeric(tasa_venta$getElementAttribute("value"))
+  
+  data.frame(
+    date = Sys.Date(),
+    bank = "Banco Popular",
+    buy = compra,
+    sell = venta
+  )
+}
+
+#' Descarga la tasa de cambio de Banco BHD
+#' @export
+tasa_dolar_bhd <- function(selenium_client) {
+  
+  selenium_client$navigate("https://www.bhdleon.com.do/wps/portal/BHD/Inicio/")
+  selenium_client$maxWindowSize()
+  
+  tasas_banner <- selenium_client$findElement(
+    using = "css selector", 
+    "#footer > section > div > ul > li:nth-child(5)"
+  )
+  tasas_banner$clickElement()
+  
+  tasa_compra <- selenium_client$findElement(
+    using = "css selector",
+    "#TasasDeCambio > table > tbody > tr:nth-child(2) > td:nth-child(2)"
+  )
+  tasa_venta <- selenium_client$findElement(
+    using = "css selector",
+    "#TasasDeCambio > table > tbody > tr:nth-child(2) > td:nth-child(3)"
+  )
+  
+  
+  tasa_venta$getElementText()
+  
+  compra <- parse_number(unlist(tasa_compra$getElementText()))
+  venta <- parse_number(unlist(tasa_venta$getElementText()))
+  
+  data.frame(
+    date = Sys.Date(),
+    bank = "BHD",
+    buy = compra,
+    sell = venta
+  )
 }
