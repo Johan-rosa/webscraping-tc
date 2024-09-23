af72308addd8d52f4c32baad360033b154f7a9ec
Johan Rosa <johan.rosaperez@gmail.com>
2024-09-23 15:59:45 -0400

    update test

--- scripts/test-rselenium.R
+++ scripts/test-rselenium.R
@@ -1,6 +1,7 @@
 install.packages(
   c(
     "RSelenium",
+    "glue",
     "logger"
   )
 )
@@ -8,38 +9,18 @@ install.packages(
 library(RSelenium)
 library(logger)
 
-initFun <- function(silent = TRUE, ...) {
-  browserName <- Sys.getenv("SELENIUM_BROWSER", "chrome")
-  remDr <- remoteDriver(browserName = browserName, ...)
-  
-  remDr$open(silent)
-  # set page load timeout to 20 secs
-  remDr$setTimeout(milliseconds = 20000)
-  # wait 5 secs for elements to load
-  remDr$setTimeout(type = "implicit", milliseconds = 5000)
-  
-  htmlSrc <- Sys.getenv("TEST_SERVER", "http://localhost:3000")
-  loadPage <- function(pgStr) {
-    paste0(file.path(htmlSrc, paste0(pgStr, ".html")))
-  }
-  
-  rdBrowser <- remDr$browserName
-  
-  list(remDr = remDr, rdBrowser = rdBrowser, loadPage = loadPage)
-}
-
 log_info("Init driver")
 
-init <- initFun()
-remDr <- init$remDr
-rdBrowser <- init$rdBrowser
+rD <- RSelenium::rsDriver(
+  browser = "chrome",
+  port = 4444L
+)
 
-on.exit(remDr$close())
-
-page <- "https://en.wikipedia.org/wiki/Hadley_Wickham"
+client <- rD[["client"]]
 
 log_info("Navigate")
-remDr$navigate(page)
+page <- "https://en.wikipedia.org/wiki/Hadley_Wickham"
+client$navigate(page)
 
 log_info("Get element")
 title <- remDr$findElement("css selector", ".mw-page-title-main")
