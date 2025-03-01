e7e88f9e94137f892abe7d8abe5971baf6c4c1a3
Johan Rosa <johan.rosaperez@gmail.com>
2025-02-28 22:17:57 -0400

    .

--- scripts/run_webscraping.R
+++ scripts/run_webscraping.R
@@ -41,10 +41,10 @@ tasas <- list(
   #bdi = tasa_dolar_bdi(),
   #vimenca = tasa_dolar_vimenca(client),
   #blh = tasa_dolar_blh(),
-  promerica = tasa_dolar_promerica(client),
+  #promerica = tasa_dolar_promerica(client),
   #banesco = tasa_dolar_banesco(client),
   #lafise = tasa_dolar_lafise(client),
-  ademi = tasa_dolar_ademi(client)
+  #ademi = tasa_dolar_ademi(client)
 )
 
 data <- dplyr::bind_rows(tasas)
